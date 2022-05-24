32cb860a376241ad92011c64ffbc4033fb738177
gabrieldt69 <gabrielduartedt69@gmail.com>
2022-05-24 08:29:59 -0300

    refazer

--- js/script.js
+++ js/script.js
@@ -30,26 +30,4 @@ document.getElementById('upload-csv2').addEventListener('change', () => {
     }
 });
 
-//verificação dos nomes duplicados
-document.getElementById('btn-compare-csv').addEventListener('click', () => {
-    let contador = 0;
-    let contadorVerificações = 0;
 
-    file1.data.map((elementoA) => {
-        
-        file2.data.map((elementoB) => {
-            if (elementoA[3]?.toLowerCase()?.toLowerCase() == elementoB[3]?.toLowerCase()) {
-                console.log(`${elementoA[3]?.toLowerCase()} == ${elementoB[3]?.toLowerCase()}`);
-                contador++;
-            }else{
-                console.log(`Nome da planilha A não encontrado na planilha B: ${elementoA[3]?.toLowerCase()?.toLowerCase()}`);
-            }
-
-            contadorVerificações++;
-        });
-    });
-
-    alert(
-        `Existem ${contador} nomes do arquivo A no arquivo B\nTotal de Verificações: ${contadorVerificações}\n`
-    );
-});
