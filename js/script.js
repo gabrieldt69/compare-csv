7d6e4457662003d9e910fd0bc1b502ab5228068e
gabrieldt69 <gabrielduartedt69@gmail.com>
2022-06-03 11:32:28 -0300

    versão beta da aplicação

--- js/script.js
+++ js/script.js
@@ -32,18 +32,26 @@ document.getElementById('upload-csv2').addEventListener('change', () => {
 
 //verificação dos nomes não encontrados
 document.getElementById('btn-compare-csv').addEventListener('click', () => {
-    let contador = 0;
-    let contadorVerificações = 0;
+    // cabeçalho dos arquivos csv
+    let headFile1 = file1.data.shift()
+    let headFile2 = file2.data.shift()
 
-    file1.data.map((elementoA) => {
-        file2.data.map((elementoB) => {
-            if (elementoA[3]?.toLowerCase() != elementoB[3]?.toLowerCase()) {
-                console.log(`${elementoA[3]?.toLowerCase()}`);
-                contador++;
-            }
-            contadorVerificações++;
-        });
-    });
+    // separando os nomes de cada linha em um só array
+    const file2_ids = file2.data.map((item) => item[3]?.toLowerCase())
 
-    alert(`Existem ${contador} nomes do arquivo A não encontrados no arquivo B\nTotal de Verificações: ${contadorVerificações}\n`);
+    // filtra os itens do arquivo 1 que não estão no arquivo 2
+    const result = file1.data.filter((item) => {
+      return file2_ids.indexOf(item[3]?.toLowerCase()) === -1
+    })
+
+    //filtra o resultado para mostrar apenas os nomes
+
+    let resultado = result.map(r => r[3]?.toLowerCase());
+
+    document.getElementById('result').innerHTML= 'Não encontrados: <br>' + resultado.join('<br>');
+
+    if (result?.length)
+      alert(`Existem ${result.length} nomes do arquivo A não encontrados no arquivo B\nTotal de Verificações: ${file1.data.length}\n`);
+    else
+      alert(`Todos os nomes do arquivo A estão no arquivo B\nTotal de Verificações: ${file2.data.length}\n`);
 });
